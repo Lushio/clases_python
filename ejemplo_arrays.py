344dbf9b03d2926d137f0edc1c3b9f23f944abee
Bernardo Campos Diocaretz <bcamposdiocaretz@gmail.com>
2019-05-27 16:39:55 -0400

    Create ejemplo_arrays.py

---
+++ ejemplo_arrays.py
@@ -0,0 +1,21 @@
+personas = []
+
+personas.append("Daniel")
+personas.append("Marcelo")
+personas.append("Alejandro")
+personas.append("Marco")
+
+print (personas)
+print (personas[2])
+
+frutas = ["manzana", "platano", "melon"]
+
+for x in frutas:
+    print(f"Es el dia del {x}")
+
+print ("agrego otra fruta")
+
+frutas.append("kiwi")
+
+for x in frutas:
+    print(f"Es el dia del {x}")
